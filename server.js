df7282e38164a1bd1a75c2b89631b7428193b2f9
Minhaul Abedin <55100658+MIARD@users.noreply.github.com>
2020-12-09 01:52:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var cors = require('cors');
+const app = express();
+const multer = require('multer')
+const bodyParser = require('body-parser')
 require('dotenv').config()
-
-var app = express();
-
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+const upload = multer({dest:'uploads/'});
 
+
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json);
+
+
+
+
+app.post('/api/fileanalyse',upload.any(),(req,res)=>{
+  console.log('no');
+  try{
+    let send = {name:req.files[0].originalname,type:req.files[0].mimetype,size:req.files[0].size};
+    res.json(send);
+    console.log('hello there'+req)
+  }catch(err){
+    res.send(err);
+    console.log('no here'+err);
+  }
+})
 
 
 
